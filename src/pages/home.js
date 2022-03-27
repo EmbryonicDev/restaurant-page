a316a45541c78cc2c22be652c932c2745987f78b
EmbryonicDev <95647817+EmbryonicDev@users.noreply.github.com>
2022-03-27 15:16:53 +0200

    Add main contents to DOM

--- src/pages/home.js
+++ src/pages/home.js
@@ -27,6 +27,20 @@ const home = (() => {
   contactBtn.innerText = 'Contact';
   navWrap.append(homeBtn, menuBtn, contactBtn);
 
+  // main content
+  const content = document.getElementById('content'),
+    textOne = document.createElement('p');
+  textOne.innerText = "Congratulations! Your search is over, you've \njust found the best joint to cure your munchies! \nOur tasty food & beverages are sure to tantalize \nyour tastebuds...";
+
+  const homeImg = document.createElement('img');
+  homeImg.src = "../src/assets/images/meat-on-grill.jpg";
+  homeImg.alt = "Meat on Grill";
+  homeImg.classList.add("homeImg");
+
+  const textTwo = document.createElement('p');
+  textTwo.innerText = "Visit us to feast like a king or place your order \n online now...";
+
+  content.append(textOne, homeImg, textTwo);
 })();
 
 export { home };
