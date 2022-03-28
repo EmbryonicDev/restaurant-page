3b343ddf0650f2c2d162d6daa2924366e15aaa5d
EmbryonicDev <95647817+EmbryonicDev@users.noreply.github.com>
2022-03-28 10:55:41 +0200

    Move siteHeader / siteFooter to globalFunctions & import again

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,33 +1,8 @@
-const home = (() => {
+import { addHeader } from "../functions/globalFuntions";
+import { addFooter } from "../functions/globalFuntions";
 
-  const siteHeader = (() => {
-    // header
-    const header = document.createElement('header');
-    header.id = "myHeader";
-    // insert header as body's first child
-    document.body.insertAdjacentElement("afterbegin", header);
-  
-    // header wrap
-    const headerWrap = document.createElement('div');
-    headerWrap.classList.add('headerDiv');
-    header.appendChild(headerWrap);
-    // header Title
-    const headerTitle = document.createElement('H1');
-    headerTitle.innerText = "🍔 Master Munchies 🍧";
-    // nav wrap
-    const navWrap = document.createElement('div');
-    navWrap.classList.add('navWrap');
-    headerWrap.append(headerTitle, navWrap)
-  
-    // buttons
-    const homeBtn = document.createElement('button'),
-      menuBtn = document.createElement('button'),
-      contactBtn = document.createElement('button');
-    homeBtn.innerText = 'Home';
-    menuBtn.innerText = 'Menu';
-    contactBtn.innerText = 'Contact';
-    navWrap.append(homeBtn, menuBtn, contactBtn);
-  })();
+const home = (() => {
+  addHeader();
 
   // main content
   const content = document.getElementById('content'),
@@ -44,25 +19,7 @@ const home = (() => {
 
   content.append(textOne, homeImg, textTwo);
 
-  const siteFooter = (() => {
-    // footer
-    const footer = document.createElement('FOOTER');
-    footer.id = "Myfooter";
-    footer.innerText = "© Embryonic Dev 2022";
-    const footerSpan = document.createElement('span'),
-      footerLogo = document.createElement('img');
-    footerLogo.src = "../src/assets/icons/tree-logo.svg";
-  
-    document.body.appendChild(footer);
-    footer.append(footerSpan);
-    footerSpan.append(footerLogo);
-  })();
-
-  // return {
-  //   siteHeader,
-  //   siteFooter
-  // }
-
+  addFooter();
 })();
 
 export { home };
