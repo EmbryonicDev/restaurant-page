96925ed9088bb2374451e976f847416788eb6447
EmbryonicDev <95647817+EmbryonicDev@users.noreply.github.com>
2022-03-29 08:23:39 +0200

    Lay out DOM structure for menu contents & export

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,5 +1,53 @@
-const menu = (() => {
+import { addHeader } from "../functions/globalFuntions";
+import { addFooter } from "../functions/globalFuntions";
 
+const addMenuContent = () => {
+  // main content
+  const content = document.getElementById('content'),
+    contentDiv = document.createElement('div');
+  contentDiv.id = 'menuContent'
+
+  // menu headings
+  const menuHeadings = ['Starters', 'Main Courses', 'Desert', 'Drinks']
+
+  const addMenuWraps = (subSection, item, sectionHeading, icon, namePrice, itemHeading, price, itemText) => {
+    sectionHeading = document.createElement('H3')
+    sectionHeading.innerText = subSection.toString();
+
+    subSection = document.createElement('div');
+    subSection.classList.add('subSection');
+
+    item = document.createElement('div');
+    item.classList.add('menuItem');
+
+    icon = document.createElement('div');
+    icon.classList.add('menuIcon');
+
+    namePrice = document.createElement('div');
+    namePrice.classList.add('namePrice');
+
+    itemHeading = document.createElement('H4');
+    price = document.createElement('span');
+    itemText = document.createElement('p');
+
+    contentDiv.append(subSection);
+    subSection.append(sectionHeading, item);
+    item.append(icon, namePrice, itemText);
+    namePrice.append(itemHeading, price);
+  }
+
+  for(let i = 0; i < menuHeadings.length; i++) {
+    addMenuWraps(menuHeadings[i], [i], [i], [i], [i], [i], [i], [i])
+  }
+
+  content.append(contentDiv)
+}
+
+const menu = (() => {
+  addMenuContent();
 })();
 
-export { menu };
+export {
+  menu,
+  addMenuContent
+};
