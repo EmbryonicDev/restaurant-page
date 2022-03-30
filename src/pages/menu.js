43563fcf5213b20057206ccc435eaba08c542749
EmbryonicDev <95647817+EmbryonicDev@users.noreply.github.com>
2022-03-30 09:23:25 +0200

    Move menuHeadings[] down to for loop using it

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,28 +1,27 @@
 const addMenuContent = () => {
-  const menuHeadings = ['Starters', 'Main Courses', 'Desert', 'Drinks']
-
+  
   // create DOM elements
   const addMenuWraps = (subSection, item, sectionHeading, icon, namePrice, itemHeading, price, itemText) => {
     let counter = 0;
     const content = document.getElementById('content'),
       contentDiv = document.createElement('div');
     contentDiv.id = 'menuContent'
-
+    
     sectionHeading = document.createElement('H3')
     sectionHeading.innerText = subSection
 
     subSection = document.createElement('div');
     subSection.classList.add('subSection');
-
+    
     item = document.createElement('div');
     item.classList.add('menuItem');
 
     icon = document.createElement('div');
     icon.classList.add('menuIcon');
-
+    
     namePrice = document.createElement('div');
     namePrice.classList.add('namePrice');
-
+    
     itemHeading = document.createElement('H4');
     price = document.createElement('span');
     itemText = document.createElement('p');
@@ -32,7 +31,7 @@ const addMenuContent = () => {
     subSection.append(sectionHeading, item);
     item.append(icon, namePrice, itemText);
     namePrice.append(itemHeading, price);
-
+    
     // create 3 additional menu items
     while(counter < 3) {
       let myClone = item.cloneNode(true);
@@ -40,9 +39,10 @@ const addMenuContent = () => {
       console.log(myClone);
       counter++;
     }
-
+    
   }
-
+  
+  const menuHeadings = ['Starters', 'Main Courses', 'Desert', 'Drinks']
   for (let i = 0; i < menuHeadings.length; i++) {
     addMenuWraps(menuHeadings[i], [i], [i], [i], [i], [i], [i], [i])
   }
