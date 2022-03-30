048cc4b2cdae51362fe21b1622b1bff1fc140864
EmbryonicDev <95647817+EmbryonicDev@users.noreply.github.com>
2022-03-30 09:36:55 +0200

    Move content & contentDiv out of addMenuWraps. Add extra unique class to each menu subsection

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,48 +1,50 @@
 const addMenuContent = () => {
+  const content = document.getElementById('content'),
+  contentDiv = document.createElement('div');
+  contentDiv.id = 'menuContent'
+  content.append(contentDiv);
   
+  const menuHeadings = ['Starters', 'Main-Courses', 'Desert', 'Drinks']
+  let counterTwo = 0;
+
   // create DOM elements
   const addMenuWraps = (subSection, item, sectionHeading, icon, namePrice, itemHeading, price, itemText) => {
     let counter = 0;
-    const content = document.getElementById('content'),
-      contentDiv = document.createElement('div');
-    contentDiv.id = 'menuContent'
-    
+
     sectionHeading = document.createElement('H3')
     sectionHeading.innerText = subSection
 
     subSection = document.createElement('div');
-    subSection.classList.add('subSection');
-    
+    subSection.classList.add('subSection', menuHeadings[counterTwo]);
+
     item = document.createElement('div');
     item.classList.add('menuItem');
 
     icon = document.createElement('div');
     icon.classList.add('menuIcon');
-    
+
     namePrice = document.createElement('div');
     namePrice.classList.add('namePrice');
-    
+
     itemHeading = document.createElement('H4');
     price = document.createElement('span');
     itemText = document.createElement('p');
 
-    content.append(contentDiv)
     contentDiv.append(subSection);
     subSection.append(sectionHeading, item);
     item.append(icon, namePrice, itemText);
     namePrice.append(itemHeading, price);
-    
+
     // create 3 additional menu items
     while(counter < 3) {
       let myClone = item.cloneNode(true);
       subSection.append(myClone);
-      console.log(myClone);
       counter++;
     }
-    
+
+    counterTwo++;
   }
-  
-  const menuHeadings = ['Starters', 'Main Courses', 'Desert', 'Drinks']
+
   for (let i = 0; i < menuHeadings.length; i++) {
     addMenuWraps(menuHeadings[i], [i], [i], [i], [i], [i], [i], [i])
   }
