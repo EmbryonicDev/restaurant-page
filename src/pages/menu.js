03b184796ce3b227a554b1602ca7599e407bf0b6
EmbryonicDev <95647817+EmbryonicDev@users.noreply.github.com>
2022-04-01 12:58:11 +0200

    Create addMenuData to populate menuItems
    Add names to menuData.starters
    Create object: required containing data for addMenuData()
    Delete old code used to populate menuItems

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -30,6 +30,7 @@ const addMenuContent = () => {
     classForHeading: ['starters', 'mainCourses', 'deserts', 'drinks'],
     starters: {
       icons: ['🧆', '🍿', '🍣', '🥪'],
+      name: ['Falafel', 'Popcorn', 'Sushi', 'Sandwidch'],
       price: ['$3', '$2', '$5', '$3.50'],
       text: ["Secret Middle Eastern recipe", "Add your own spices", "Fresh fish used", "Ham, cheese, and tomatto"]
     },
@@ -76,171 +77,34 @@ const addMenuContent = () => {
     }
   })();
 
-  console.log(menuData.starters.icons[0]);
+  const required = {
+    foodCategory: ['starters', 'mainCourses', 'deserts', 'drinks'],
+    selector: ['.starters', '.mainCourses', '.deserts', '.drinks'],
+    targetGroup: ['.menuIcon', 'H4', 'span', 'p'],
+    itemContent: ['icons', 'name', 'price', 'text'],
+  }
 
-  let itemCounter = 0;
-  // // Add starter info
-  // (() => {
-    // add starter food icons
-    const starterIcons = ['🧆', '🍿', '🍣', '🥪'];
-    let starterWrap = document.querySelector('.starters');
-    starterWrap.querySelectorAll('.menuItem').forEach(element => {
-      element.innerText = menuData.starters.icons[itemCounter];
-      itemCounter++;
-    });
+  const addMenuData = (selector, targetGroup, foodCategory, itemContent) => {
+    const container = document.querySelector(selector);
+    let counter = 0;
 
-  //   // add starter food names
-  //   itemCounter = 0;
-  //   const starterName = ['Falafel', 'Popcorn', 'Sushi', 'Sandwidch'];
-  //   starterWrap.querySelectorAll('H4').forEach(element => {
-  //     element.innerText = starterName[itemCounter];
-  //     itemCounter++;
-  //   });
+    container.querySelectorAll(targetGroup).forEach(item => {
+      item.innerText = menuData[foodCategory][itemContent][counter];
+      counter++;
+    })
+  };
 
-  //   // add starter food prices
-  //   itemCounter = 0;
-  //   const starterPrice = ['$3', '$2', '$5', '$3.50'];
-  //   starterWrap.querySelectorAll('span').forEach(element => {
-  //     element.innerText = starterPrice[itemCounter];
-  //     itemCounter++;
-  //   });
-
-  //   // add starter text
-  //   itemCounter = 0;
-  //   const starterText = [
-  //     "Secret Middle Eastern recipe",
-  //     "Add your own spices",
-  //     "Fresh fish used",
-  //     "Ham, cheese, and tomatto"
-  //   ];
-  //   starterWrap.querySelectorAll('p').forEach(element => {
-  //     element.innerText = starterText[itemCounter];
-  //     itemCounter++;
-  //   });
-  // })();
-
-  // // Add main course info
-  // (() => {
-  //   // add main-course food icons
-  //   const mainCourseIcons = ['🍔', '🌭', '🍝', '🥩'];
-  //   itemCounter = 0;
-  //   let mainCourseWrap = document.querySelector('.main-courses');
-  //   mainCourseWrap.querySelectorAll('.menuIcon').forEach(element => {
-  //     element.innerHTML = mainCourseIcons[itemCounter];
-  //     itemCounter++
-  //   });
-
-  //   // add main course food names
-  //   itemCounter = 0;
-  //   const mainCourseName = ['Burger', 'Hotdog', 'Spaghetti', 'Steak'];
-  //   mainCourseWrap.querySelectorAll('H4').forEach(element => {
-  //     element.innerText = mainCourseName[itemCounter];
-  //     itemCounter++;
-  //   });
-
-  //   // add main course food prices
-  //   itemCounter = 0;
-  //   const mainCoursePrice = ['$7.50', '$6.50', '$7', '$9.50'];
-  //   mainCourseWrap.querySelectorAll('span').forEach(element => {
-  //     element.innerText = mainCoursePrice[itemCounter];
-  //     itemCounter++;
-  //   });
-
-  //   // add main course text
-  //   itemCounter = 0;
-  //   const mainCourseText = [
-  //     "Cheese burger with tomatto and lettuce",
-  //     "Homemade mustard added",
-  //     "With Bolognese sauce",
-  //     "500g pure beef"
-  //   ];
-  //   mainCourseWrap.querySelectorAll('p').forEach(element => {
-  //     element.innerText = mainCourseText[itemCounter];
-  //     itemCounter++;
-  //   });
-  // })();
-
-  // // Add desert info
-  // (() => {
-  //   // Add desert icons
-  //   const desertIcons = ['🥞', '🧇', '🍨', '🥧']
-  //   itemCounter = 0;
-  //   let desertsWrap = document.querySelector('.desert');
-  //   desertsWrap.querySelectorAll('.menuIcon').forEach(element => {
-  //     element.innerHTML = desertIcons[itemCounter];
-  //     itemCounter++
-  //   });
-
-  //   // add desert names
-  //   itemCounter = 0;
-  //   const desertsName = ['Pancakes', 'Waffle', 'Ice Cream', 'Pie'];
-  //   desertsWrap.querySelectorAll('H4').forEach(element => {
-  //     element.innerText = desertsName[itemCounter];
-  //     itemCounter++;
-  //   });
-
-  //   // add desert prices
-  //   itemCounter = 0;
-  //   const desertsPrice = ['$4.50', '$4', '$3.50', '$4'];
-  //   desertsWrap.querySelectorAll('span').forEach(element => {
-  //     element.innerText = desertsPrice[itemCounter];
-  //     itemCounter++;
-  //   });
-
-  //   // add desert text
-  //   itemCounter = 0;
-  //   const desertsText = [
-  //     "Drenched in maple syrup",
-  //     "Served with ice cream",
-  //     "Vanilla, cholocolate, or strawberry",
-  //     "Chocolate chip pecan pie"
-  //   ];
-  //   desertsWrap.querySelectorAll('p').forEach(element => {
-  //     element.innerText = desertsText[itemCounter];
-  //     itemCounter++;
-  //   });
-  // })();
-
-  // // Add drinks info
-  // (() => {
-  //   // Add drinks icons
-  //   const drinksIcons = ['🍹', '🍺', '🍷', '☕']
-  //   itemCounter = 0;
-  //   let drinksWrap = document.querySelector('.drinks');
-  //   drinksWrap.querySelectorAll('.menuIcon').forEach(element => {
-  //     element.innerHTML = drinksIcons[itemCounter];
-  //     itemCounter++
-  //   });
-
-  //   // add drinks names
-  //   itemCounter = 0;
-  //   const drinksName = ['Fruit Coctail', 'Beer', 'Wine', 'Coffee'];
-  //   drinksWrap.querySelectorAll('H4').forEach(element => {
-  //     element.innerText = drinksName[itemCounter]
-  //     itemCounter++;
-  //   });
-
-  //   // add drinks prices
-  //   itemCounter = 0;
-  //   const drinksPrice = ['$2.50', '$3', '$2.50', '$2.50'];
-  //   drinksWrap.querySelectorAll('span').forEach(element => {
-  //     element.innerText = drinksPrice[itemCounter]
-  //     itemCounter++;
-  //   });
-
-  //   // add drink text
-  //   itemCounter = 0;
-  //   const drinksText = [
-  //     "Orange, mango, apple",
-  //     "Home brewed",
-  //     "Red or white",
-  //     "Cappuccino or Late Macchiato"
-  //   ];
-  //   drinksWrap.querySelectorAll('p').forEach(element => {
-  //     element.innerText = drinksText[itemCounter];
-  //     itemCounter++;
-  //   })
-  // })();
+  (() => {
+    let counter = 0;
+    for (let i = 0; i < required.foodCategory.length; i++) {
+      addMenuData(required.selector[0], required.targetGroup[i], required.foodCategory[0], required.itemContent[i]);
+      addMenuData(required.selector[1], required.targetGroup[i], required.foodCategory[1], required.itemContent[i]);
+      addMenuData(required.selector[2], required.targetGroup[i], required.foodCategory[2], required.itemContent[i]);
+      addMenuData(required.selector[3], required.targetGroup[i], required.foodCategory[3], required.itemContent[i]);
+      counter++;
+      console.log({counter});
+    }
+  })();
 }
 
 const menu = () => {
