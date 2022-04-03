87fd0b4a35574bfd5a8378ef725180c648a28123
EmbryonicDev <95647817+EmbryonicDev@users.noreply.github.com>
2022-04-03 11:34:13 +0200

    Incorporate elFactory on home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,25 +1,16 @@
 import { addHeader } from "../functions/globalFuntions";
 import { addFooter } from "../functions/globalFuntions";
+import { elFactory } from "../functions/globalFuntions";
 
 const addHomeContent = () => {
   // main content
-  const content = document.getElementById('content'),
-    contentDiv = document.createElement('div');
-  contentDiv.id = 'homeContent'
+  const content = document.getElementById('content');
 
-  const textOne = document.createElement('p');
-  textOne.innerText = "Congratulations! Your search is over, you've just found the best joint to cure your munchies! Our tasty food & beverages are sure to tantalize your tastebuds...";
-
-  const homeImg = document.createElement('img');
-  homeImg.src = "../src/assets/images/meat-on-grill.jpg";
-  homeImg.alt = "Meat on Grill";
-  homeImg.classList.add("homeImg");
-
-  const textTwo = document.createElement('p');
-  textTwo.innerText = "Visit us to feast like a king or place your order online now...";
-
-  content.append(contentDiv)
-  contentDiv.append(textOne, homeImg, textTwo);
+  elFactory('div', { id: 'homeContent' }, content,
+    elFactory('p', '', '', "Congratulations! Your search is over, you've just found the best joint to cure your munchies! Our tasty food & beverages are sure to tantalize your tastebuds..."),
+    elFactory('img', { src: "../src/assets/images/meat-on-grill.jpg", alt: "Meat on Grill" }, ''),
+    elFactory('p', '', '', "Visit us to feast like a king or place your order online now...")
+  )
 }
 
 const home = (() => {
@@ -28,7 +19,7 @@ const home = (() => {
   addFooter();
 })();
 
-export { 
+export {
   home,
   addHomeContent
 };
